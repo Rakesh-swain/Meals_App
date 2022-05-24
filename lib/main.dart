c66fc02a127de8e6047b51ef7deca3621cdbb92a
Rakesh005 <srakeshswain005@gmail.com>
2022-05-24 17:50:33 +0530

    add static const property to routename

--- lib/main.dart
+++ lib/main.dart
@@ -27,8 +27,12 @@ class MyApp extends StatelessWidget {
                 fontFamily: 'RobotoCondensed',
                 fontWeight: FontWeight.bold)),
       ),
-      home: CategoriesScreen(),
-      routes: {'/category-meals': (ctx) => CategoryMealsScreen()},
+      // home: CategoriesScreen(),
+      initialRoute: '/', //default is '/'
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen()
+      },
     );
   }
 }
