6a2fd5d0f6c3fbcbb22b7d8faf17ce6de4e2aa57
Rakesh005 <srakeshswain005@gmail.com>
2022-05-25 17:18:46 +0530

    displaying ingredients in meal detailed screen

--- lib/screeens/meal_detail_screen.dart
+++ lib/screeens/meal_detail_screen.dart
@@ -1,14 +1,55 @@
 import 'package:flutter/material.dart';
+import '../dummy_data.dart';
 
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
-      appBar: AppBar(title: Text('$mealId')),
-      body: Center(
-        child: Text('The meal -$mealId!'),
+      backgroundColor: Colors.white,
+      appBar: AppBar(title: Text('${selectedMeal.title}')),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              selectedMeal.imageurl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            height: 150,
+            width: 300,
+            child: ListView.builder(
+              itemBuilder: (ctx, index) => Card(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Text(selectedMeal.ingredients[index]),
+                ),
+                color: Theme.of(context).accentColor,
+              ),
+              itemCount: selectedMeal.ingredients.length,
+            ),
+          ),
+        ],
       ),
     );
   }
